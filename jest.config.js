2918120d4f901984cb31a9c9136299b08a532cd6
YamiCueto <yamidcuetomazo@gmail.com>
2024-08-08 09:55:22 -0500

    add config for test using jest and the bundle for generate report of coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,28 @@
 module.exports = {
   preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  testEnvironment: 'jest-environment-jsdom',
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@env': '<rootDir>/src/environments',
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/src/test.ts',
+  ],
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: './html-report',
+      filename: 'report.html',
+      expand: true,
+    }],
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text-summary'],
 };
